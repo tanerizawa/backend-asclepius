925c52d413b38ea4fb2d26e04d906dd0e052ef2c
tanerizawa <studiomalaka@gmail.com>
2024-03-29 00:41:24 +0700

    Add new files and update existing files

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,5 @@
 // TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'YOUR_BASE_URL';
+const BASE_URL = 'http://34.101.160.77:3000';
 
 const ENDPOINT = {
   predict: `${BASE_URL}/predict`,
